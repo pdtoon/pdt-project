496924896f12c27b568dfc8d773a77da23b9b862
zx4545zx <zx4545zx>
2022-10-23 18:58:54 +0700

    update 23/10/2565

--- components/Booking/PaymentCard.js
+++ components/Booking/PaymentCard.js
@@ -1,52 +1,75 @@
-import React from 'react'
+import React from "react";
 
 const PaymentCard = () => {
   return (
     <>
       <div>
         <h4 className="mb-3">Payment</h4>
-        <form className="needs-validation" novalidate="">
+        <form className="needs-validation">
           <hr className="my-4" />
           <div className="my-3">
             <div className="form-check">
-              <input id="kbank" name="paymentMethod" type="radio" className="form-check-input" />
-              <label className="form-check-label" htmlFor="credit">KBANK</label>
+              <input
+                id="kbank"
+                name="paymentMethod"
+                type="radio"
+                className="form-check-input"
+              />
+              <label className="form-check-label" htmlFor="credit">
+                KBANK
+              </label>
             </div>
             <div className="form-check">
-              <input id="scb" name="paymentMethod" type="radio" className="form-check-input" />
-              <label className="form-check-label" htmlFor="debit">SCB</label>
+              <input
+                id="scb"
+                name="paymentMethod"
+                type="radio"
+                className="form-check-input"
+              />
+              <label className="form-check-label" htmlFor="debit">
+                SCB
+              </label>
             </div>
             <div className="form-check">
-              <input id="paybycash" name="paymentMethod" type="radio" className="form-check-input" />
-              <label className="form-check-label" htmlFor="paypal">Pay By Cash</label>
+              <input
+                id="paybycash"
+                name="paymentMethod"
+                type="radio"
+                className="form-check-input"
+              />
+              <label className="form-check-label" htmlFor="paypal">
+                Pay By Cash
+              </label>
             </div>
           </div>
 
           <div className="row gy-3">
             <div className="col-md-6">
-              <label htmlFor="amount" className="form-label">Amount</label>
+              <label htmlFor="amount" className="form-label">
+                Amount
+              </label>
               <input type="text" className="form-control" id="amount" />
-              <div className="invalid-feedback">
-                Amount is required
-              </div>
+              <div className="invalid-feedback">Amount is required</div>
             </div>
 
             <div className="col-md-6">
-              <label htmlFor="slip" className="form-label">Slip</label>
+              <label htmlFor="slip" className="form-label">
+                Slip
+              </label>
               <input type="file" className="form-control" id="slip" />
-              <div className="invalid-feedback">
-                Slip is required
-              </div>
+              <div className="invalid-feedback">Slip is required</div>
             </div>
           </div>
 
           <hr className="my-4" />
 
-          <button className="w-100 btn btn-primary btn-lg" type="submit">Confirm</button>
+          <button className="w-100 btn btn-primary btn-lg" type="submit">
+            Confirm
+          </button>
         </form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default PaymentCard
+export default PaymentCard;
--- pages/login.js
+++ pages/login.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import Cookies from 'js-cookie'
-import { useRouter } from 'next/router'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
 
 const Login = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   const onLogin = () => {
-    let role = 'user'
-    Cookies.set('role', role)
+    let role = "admin";
+    Cookies.set("role", role);
 
-    if (role == 'admin') {
-      router.replace("/admin")
+    if (role == "admin") {
+      router.replace("/admin");
     } else {
-      router.replace("/")
+      router.replace("/");
     }
-  }
+  };
 
   return (
-    <div className='p-4'>
-      <main className="form-signin m-auto" style={{ width: '500px' }}>
+    <div className="p-4">
+      <main className="form-signin m-auto" style={{ width: "500px" }}>
         <div className="card">
           <Image
             src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
@@ -34,30 +34,56 @@ const Login = () => {
           <div className="card-body">
             <div>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" />
-                <div id="emailHelp" className="form-text">We never share your email with anyone else.</div>
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                />
+                <div id="emailHelp" className="form-text">
+                  We never share your email with anyone else.
+                </div>
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                />
               </div>
               <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Check me out
+                </label>
               </div>
-              <button type="button" onClick={onLogin} className="btn btn-primary">Login</button>
+              <button
+                type="button"
+                onClick={onLogin}
+                className="btn btn-primary"
+              >
+                Login
+              </button>
             </div>
           </div>
         </div>
-        <div className='text-center m-3'>
+        <div className="text-center m-3">
           <Link href="/register">
             <a className="btn btn-link btn-lg">Create a Sign Up</a>
           </Link>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
